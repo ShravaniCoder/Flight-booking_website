e1955620d7805761bf1f294d670501d2f9f91258
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-06-04 13:36:39 +0530

    yes

--- app/_components/SearchInput.jsx
+++ app/_components/SearchInput.jsx
@@ -94,35 +94,35 @@ function SearchInput() {
         <div className="flex bg-gray-100 py-1 mb-2 sm:mb-0 px-2 divide-x">
           <div className="flex relative">
             <label
-              htmlFor="UserEmail"
+              htmlFor="name"
               className="relative block overflow-hidden border-b border-gray-200 bg-transparent pt-3 focus-within:border-blue-600"
             >
               <input
-                type="email"
-                id="UserEmail"
-                placeholder="Email"
+                type="travellers"
+                id="travellers"
+                placeholder="Travellers"
                 className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
               />
 
               <span className="absolute start-0 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
-                Email
+                Travellers
               </span>
             </label>
           </div>
           <div className="flex relative">
             <label
-              htmlFor="UserEmail"
+              htmlFor="name"
               className="relative block overflow-hidden border-b border-gray-200 bg-transparent pt-3 focus-within:border-blue-600"
             >
               <input
-                type="email"
-                id="UserEmail"
-                placeholder="Email"
+                type="class"
+                id="class"
+                placeholder="Class"
                 className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
               />
 
               <span className="absolute start-0 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
-                Email
+                Class
               </span>
             </label>
           </div>
